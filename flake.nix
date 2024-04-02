bf2ab77ac3a36e22cf8f9e96b26b31de52457894
Garrett Gonzalez-Rivas <emailforggr@gmail.com>
2024-04-01 20:50:01 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,29 @@
-# flake.nix
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    hyprland.url = "github:hyprwm/Hyprland";
-  };
+  description = "Home Manager configuration of garrettgr";
 
-  outputs = {self, nixpkgs, hyprland, ...}: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        hyprland.nixosModules.default
-        {programs.hyprland.enable = true;}
-      ];
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."garrettgr" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
 }
